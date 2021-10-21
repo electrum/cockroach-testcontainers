39f165cfe8f368661f0e294710f01d7bb3d30b77
David Phillips <david@acz.org>
2021-10-21 13:05:05 -0700

    Improve timeout handling

--- src/test/java/test/TestCockroach.java
+++ src/test/java/test/TestCockroach.java
@@ -1,8 +1,6 @@
 package test;
 
-import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.CockroachContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -10,34 +8,45 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.Duration;
 
+import static java.lang.Math.toIntExact;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class TestCockroach
 {
-    @BeforeClass(timeOut = 60_000)
+    static {
+        System.setProperty("org.slf4j.simpleLogger.levelInBrackets", "true");
+        System.setProperty("org.slf4j.simpleLogger.showThreadName", "false");
+        System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
+        System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "yyyy-MM-dd HH:mm:ss.SSSZ");
+    }
+
+    @BeforeClass(timeOut = 120_000)
     public void setup()
             throws SQLException
     {
         // run with longer timeout to allow for image download
-        verifyCockroach();
+        verifyCockroach(Duration.ofSeconds(90));
     }
 
-    @Test(timeOut = 10_000, invocationCount = 100)
+    @Test(timeOut = 60_000, invocationCount = 100)
     public void testCockroach()
             throws SQLException
     {
-        verifyCockroach();
+        verifyCockroach(Duration.ofSeconds(30));
     }
 
-    private void verifyCockroach()
+    private static void verifyCockroach(Duration duration)
             throws SQLException
     {
-        CockroachContainer container = new CockroachContainer("cockroachdb/cockroach:v21.1.11");
+        CockroachContainer container = new CockroachContainer("cockroachdb/cockroach:v21.1.11")
+                .withStartupTimeout(duration)
+                .withConnectTimeoutSeconds(toIntExact(duration.toSeconds()));
+
         container.start();
-        container.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(getClass())));
 
         try (Connection connection = container.createConnection("");
                 Statement statement = connection.createStatement();
